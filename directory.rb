931b042f30f922cc502d4773b09574c46e6c23d5
lajoiedevivre89 <emilyspencer890@hotmail.co.uk>
2019-11-17 20:15:52 +0000

    Heavily refactored: made 1D- array into a multi-dimensional array, then a hash, hard-coded cohorts into array, updated methods, renamed argument from names to students

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+students = [  # Multi-dimentional array   # then made it an array of hases
+  {name: "Dr. Hannibal Lecter", cohort: :november}, # [0][0]
+  {name: "Darth Vader", cohort: :november}, # [0][1]
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 
 # Define the methods
@@ -18,18 +18,18 @@ def print_header
   puts "-------------"
 end 
 
-def print_names(names) # Passing our students variable as names argument because
+def print_names(students) # Passing our students variable as students argument because
   students.each do |student| # methods don't have access to the local variables 
-    puts student     # defined outside them 
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"    # defined outside them  - but we are no longer only passing names
   end
 end
-
-def print_footer(names) # passing in this argument names because the methods don't 
+                           # renamed argument names to students because we are no longer passing
+def print_footer(students) # only names - but the methods still don't
   puts "Overall, we have #{students.count} great students"   # have access to the
   # we could also use students.length       # students variable defined at the top 
 end 
 
 # Call the methods
 print_header
-print_names
+print_names(students) 
 print_footer(students)
